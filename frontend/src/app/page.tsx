25aefd35fd9ec909ca3bf0c8e706b36baf2e4d1f
IsraelSI11 <elplayerhd@hotmail.es>
2024-12-03 15:38:56 +0100

    Añadiendo shadcn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-roboto)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et explicabo deserunt quae! Perspiciatis temporibus consectetur vero adipisci, eaque officia eligendi ipsum. Quis esse molestias illo ullam? Sed, libero. Sint, quisquam!</p>
+        
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
        
