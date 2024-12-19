804010ad5295e3c8295ec0730d28a60b9a2815c0
IsraelSI11 <elplayerhd@hotmail.es>
2024-12-19 12:07:28 +0100

    Añadiendo longitud máxima al input de generar con ia

--- frontend/src/components/GenerateButton.tsx
+++ frontend/src/components/GenerateButton.tsx
@@ -1,10 +1,12 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Textarea } from "./ui/textarea";
 import { generateTweetAction } from "@/app/actions/generateTweetAction";
+
+const MAX_INPUT_LENGTH = 3000;
 
 export default function GenerateButton() {
   const [generateText, setGenerateText] = useState(false);
@@ -16,14 +18,16 @@ export default function GenerateButton() {
       const data = await generateTweetAction(inputText);
       setOutputText(data.tweet);
     } catch (e) {
-        console.log(e)
-        setOutputText("Error al generar el tweet");
+      console.log(e);
+      setOutputText("Error al generar el tweet");
     }
   };
 
   return (
     <>
-      <Button onClick={()=>setGenerateText(true)}>Generar Tweet con IA</Button>
+      <Button onClick={() => setGenerateText(true)}>
+        Generar Tweet con IA
+      </Button>
       <Dialog
         open={generateText}
         onOpenChange={() => {
@@ -32,22 +36,27 @@ export default function GenerateButton() {
           setOutputText("");
         }}
       >
-        <DialogContent>
+        <DialogContent className="w-11/12 max-w-5xl h-[90vh] max-h-[90vh] flex flex-col">
           <DialogHeader>
             <DialogTitle>Generar tweet</DialogTitle>
           </DialogHeader>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-2 gap-4 flex-grow overflow-hidden">
             <Textarea
-              className="h-96 max-h-96"
+              className="h-full resize-none"
               value={inputText}
               onChange={(e) => setInputText(e.target.value)}
+              maxLength={MAX_INPUT_LENGTH}
               placeholder="Enter text to generate tweet from"
             />
-
-            <Textarea disabled className="h-96 max-h-96" value={outputText} />
+            <Textarea
+              disabled
+              className="h-full resize-none"
+              value={outputText}
+            />
           </div>
-
-          <Button onClick={generateTweet}>Generar tweet</Button>
+          <Button onClick={generateTweet} className="mt-4">
+            Generar tweet
+          </Button>
         </DialogContent>
       </Dialog>
     </>
