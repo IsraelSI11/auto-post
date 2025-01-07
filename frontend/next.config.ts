f6afbc374ffe92595221ac6878945a8d8183be30
IsraelSI11 <elplayerhd@hotmail.es>
2025-01-07 11:56:53 +0100

    Añadiendo dominio en configuración nextjs para imagenes de perfil de twitter

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    domains: ['pbs.twimg.com', 'abs.twimg.com'],
     remotePatterns: [
       {
         protocol: 'https',
